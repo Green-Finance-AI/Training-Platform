39cb84afef226bfe7f2222dda604a1c92ed6892a
Alice Lépissier <alice.lepissier@gmail.com>
2025-03-13 20:06:14 -0400

    Add favicon

--- index.php
+++ index.php
@@ -31,7 +31,10 @@
         <!-- End Google Tag Manager -->
 
     <!-- Favicon -->
-    <link href="vendor/img/favicon.ico" rel="icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/site.webmanifest">
     <!-- Bundle -->
     <link rel="stylesheet" href="vendor/css/bundle.min.css">
     <!-- Plugin Css -->
